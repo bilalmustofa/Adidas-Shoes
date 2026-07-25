d401a6b0ae8d81a0cb38a8606e23cd6f34a25949
bilalmustofa <bilalmustofa146@gmail.com>
2026-07-25 23:12:48 +0300

    style:some layout and spacing change

--- frontend/src/sections/hero/Hero.jsx
+++ frontend/src/sections/hero/Hero.jsx
@@ -6,20 +6,20 @@ function Hero() {
         <section className='w-full h-screen bg-cover bg-center flex items-center'
                   style={{backgroundImage: `url(${heroBg})`}} >
             
-            <div className="ml-8 max-w-lg text-black mt-35">
+            <div className="ml-8 text-black mt-35">
                 <h1 className="text-6xl font-black uppercase bg-gradient-to-r from-[#9A0E16] to-[#000000] bg-clip-text text-transparent">
-                  ADICLUB <br /> IS SHOWING OFF
+                  ADICLUB <br /> IS SHOWING OFF.
                 </h1>
                 
-                <p className="mt-4 text-xl max-w-md text-white">
+                <p className="mt-4 text-xl max-w-xl text-white">
                   Exclusive releases for our members and more ways to unlock those things that you love
                 </p>
                 
                 <div  className="mt-5 flex flex-col gap-6 text-white">
-                  <a href="#"  className="text-3xl font-bold hover:text-yellow-300">
+                  <a href="#"  className="max-w-50 text-3xl font-bold hover:text-yellow-300">
                     → SEE MORE
                   </a>
-                  <a href="#"  className="text-3xl font-bold text-white hover:text-yellow-300">
+                  <a href="#"  className="max-w-50 text-3xl font-bold hover:text-yellow-300">
                     → BUY NOW
                   </a>
                 </div>
