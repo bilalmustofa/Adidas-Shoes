83337832b0a6b3cd37ab6a5aa3ab0ff41d0b041c
bilalmustofa <bilalmustofa146@gmail.com>
2026-07-26 15:58:45 +0300

    feat: add ProductCard component and product images and implement featured products section

---
+++ frontend/src/components/productCard/ProductCard.jsx
@@ -0,0 +1,35 @@
+import { FiHeart } from "react-icons/fi";
+import blackShoes from '../../assets/images/product-imgs/Sneaker.png'
+
+function ProductCard() {
+  return (
+    <>
+        <div>
+            <div className="relative bg-gray-100 cursor-pointer">
+                <img src={blackShoes} alt="Shoes" />
+                <button className="absolute top-4 right-4 text-2xl cursor-pointer">
+                    <FiHeart />
+                </button>
+            </div>
+
+            <div  className="mt-3">
+                <h4 className="font-semibold">$170.00</h4>
+
+                <p className="text-gray-500 text-sm">
+                    Free Shipping
+                </p>
+
+                <h3 className="font-bold mt-4">
+                    Sneaker X
+                </h3>
+
+                <p className="text-gray-500">
+                    Sneaker Style
+                </p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default ProductCard;
--- frontend/src/sections/products/Product.jsx
+++ frontend/src/sections/products/Product.jsx
@@ -1,8 +1,29 @@
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import ProductCard from "../../components/productCard/ProductCard.jsx";
 
 function Product() {
   return (
     <>
-        
+        <section className="max-w-7xl mx-auto px-8 py-10">
+
+          <div className="flex items-center justify-between">
+
+            <h2 className="text-3xl font-bold">adiWeek</h2>
+
+            <div className="flex items-center gap-3"> 
+              <FiChevronLeft size={25} className="text-2xl hover:text-gray-500 transition-colors cursor-pointer" />
+              <FiChevronRight size={25} className="text-2xl hover:text-gray-500 transition-colors cursor-pointer" />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-4 gap-6 mt-8 mx-auto max-w-7xl">
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+          </div>
+
+        </section>
     </>
   )
 }
